e1c80e76c4c14d3884df07785a99826f165c30a4
andreyokda <andreyokda@yandex.ru>
2025-02-20 17:41:43 +0300

    Обновление проекта

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,43 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: "./src/script.js",
+  entry: './src/script.js', // Файл входа
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true
+    path: path.resolve(__dirname, 'dist'), // Куда кладём билд
+    filename: 'bundle.js',
+    clean: true, // Очищает dist перед сборкой
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Указываем исходный HTML
+      filename: 'index.html', // Указываем имя выходного файла
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', // Имя файла CSS
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/assets', to: 'assets' }, // Копируем изображения
+      ],
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        type: "asset/resource",
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
         generator: {
-          filename: "assets/[name][ext]"
-        }
-      }
-    ]
+          filename: 'assets/[name][ext]',
+        },
+      },
+    ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "styles.css"
-    })
-  ],
-  devServer: {
-    static: "./dist",
-    hot: true
-  }
+  mode: 'production',
 };
